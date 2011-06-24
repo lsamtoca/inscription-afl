4ccd20732a66501b3a7799c53c30bde0be5c252c
luigi.santocanale <luigi.santocanale@lif.univ-mrs.fr>
2011-06-24 20:54:40 +0000

    Ajoute' REAME renome' des fichiers

---
+++ README.txt
@@ -0,0 +1,16 @@
+Liste des fichier dans le projet
+
+1) partage.php :
+du code partagé par les autres fichiers
+
+2) AFLdb.php
+fichier contenant les données du db du serveur 
+(en particulier login et mdp)
+à cacher quelques part
+
+3) index.php
+pour pouvoir exemplifier le programme
+
+4) Liste_regates.php
+Liste des regates disponibles à l'inscription
+(pour pouvoir exemplifier le programme)
--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require "partage.php";
 html_pre("Inscription aux regates AFL : page de test");
 ?>
 
-<h2>Role : administrateur système</h2>
+<h2>Role : administrateur du programme (Luigi, David ?)</h2>
 
 <ul>
    <li> <A href="admin/init.php">initialiser la base des données</A> </li>
@@ -11,23 +11,25 @@ html_pre("Inscription aux regates AFL : page de test");
  </ul>
  
 
-<h2>Rôle : administrateur des clubs (Martine ?)</h2>
+<h2>Rôle : administrateur AFL (Martine, Pierre ?)</h2>
 
 <ul>
    <li> <A href="Admin.php">gérer les clubs (et/où les évenements ?)</A> </li>
 </ul>
 
-<h2>Rôle : club </h2>
+<h2>Rôle : club (organisateur d'une régate)</h2>
 
 <ul>
-   <li> <A href="Club.php">gérer les régates</A> </li>
-   <li> <A href="Regate.php">gérer une régate (II ?)</A> </li>
+   <li> <A href="Regate.php">gérer une régate</A> </li>
 </ul>
 
-<h2>Tests regatiers</h2>
+<h2>Rôle : coureur</h2>
 
 <ul>
-   <li> <A href="Formulaire.php">se pré-inscrire (à une régate?)</A> </li>
+   <li> <A href="Liste_regates.php">liste des régates</A>  ouvertes à l'inscription. <br>
+      Cette liste n'est pas nécessairement accessible sur le site AFL, mais ici sert pour tester.
+    </li>
+<!--   <li> <A href="Formulaire.php">se pré-inscrire (à une régate?)</A> </li>-->
    <li> <A href="Confirmation.php">se confirmer (à une régate?)</A> </li>
    <li> <A href="Annullation.php">annuler sa participation (à une régate?)</A> </li>
  </ul>
