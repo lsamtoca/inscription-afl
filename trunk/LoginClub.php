1b5447df86c80f0febe752a0c68a1579fb4363d6
luigi.santocanale@lif.univ-mrs.fr <luigi.santocanale@lif.univ-mrs.fr@6b252061-a63d-6baa-71f7-6f238446e408>
2011-08-31 14:18:05 +0000

    A bunch of little work to adapt the project to ovh
    M    Inscription.php
    M    deconnexion.php
    M    LoginClub.php
    D    deconnexionAdmin.php
    M    partage.php
    M    Admin.php

--- trunk/LoginClub.php
+++ trunk/LoginClub.php
@@ -49,16 +49,11 @@
 			}
 		}
 	}
+
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Préinscription</title>
-</head>
+<?php xhtml_pre("Géstion de la régate, login"); ?>
 
-<body>
 <form action="" method="post">
 	<fieldset>
 	<legend>Gérer Votre Régate</legend>
@@ -72,5 +67,5 @@
 </fieldset>
 
 </form>
-</body>
-</html>
+
+<?php xhtml_post(); ?>
